96a8aa10a889256d9f105b743f07c0e48135d02b
Julien Gracia <gracia.julien@gmail.com>
2022-12-08 21:17:59 +0100

    eighth day

--- src/day08/index.ts
+++ src/day08/index.ts
@@ -1,59 +1,142 @@
 import run from "aocrunner";
 
+
 const parseInput = (rawInput: string) => rawInput;
 
-const countEdgesTrees = (rows: string[]) => {
-  return (rows[0].length) * 2 + (rows.length - 2) * 2;
-}
+const isEdgeTree = (
+  rowIndex: number,
+  colIndex: number,
+  trees: number[][],
+) => (
+  (!rowIndex || rowIndex === trees.length - 1) ||
+  (!colIndex || colIndex === trees[rowIndex].length - 1)
+);
 
+const beforeTrees = (rowOrColumn: number[], index: number) => (
+  rowOrColumn.slice(0, index).reverse()
+);
+
+const afterTrees = (rowOrColumn: number[], index: number) => (
+  rowOrColumn.slice(index + 1)
+);
+
+const isShorterThan = (value: number) => (tree: number) => tree < value;
+
+const neighborsShortThan = (neighbors: number[][], tree: number) => (
+  neighbors.some((row) => row.every(isShorterThan(tree)))
+);
+
+const rotateClockwise = <T>(matrix: T[][]) => (
+  matrix[0].map((_value, index) => (
+    matrix.map((row) => row[index]).reverse()
+  ))
+);
+
+const getRowNeighbors = (
+  row: number[],
+  index: number,
+) => [beforeTrees(row, index), afterTrees(row, index)];
+
+const getColumnNeighbors = (
+  column: number[],
+  index: number,
+) => getRowNeighbors(column, (column.length - index) - 1);
+
+const getVisibleTrees = (trees: number[][]) => {
+
+  let visible = 0;
+
+  const columns = rotateClockwise(trees);
+  
+  trees.forEach((row, rowIndex) => {
+    row.forEach((tree, colIndex) => {
+      if (isEdgeTree(rowIndex, colIndex, trees)) return visible += 1;
+
+      const neighbors = [
+        ...getRowNeighbors(row, colIndex),
+        ...getColumnNeighbors(
+          columns[colIndex],
+          rowIndex,
+        ),
+      ];
+      if (neighborsShortThan(neighbors, tree)) return visible += 1;
+    });
+  });
+  return visible;
+};
 
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  let visibleTreesCount = 0;
+  const forest = input.split("\n")
+    .map(rows => rows.split('')
+      .map(Number));
 
-  const rows = input.split("\n");
+  const visibleTrees = getVisibleTrees(forest);
 
-  // Count tree at edges (always visible)
-  visibleTreesCount += countEdgesTrees(rows);
-    
-  // Count interior trees visibles
-  const forest: number[][] = [];
-  
-  for(let row of rows) {
-    const rowAr = row.split("");
-    console.log(rowAr);
-    
-  }
-  
-  console.log(forest);
-  
-
-  console.log(visibleTreesCount);
-  
-  return visibleTreesCount.toString();
+  return visibleTrees.toString();
 };
+
+const visibleNeighborTrees = (neighbors: number[], tree: number) => (
+  neighbors.slice(0, neighbors.findIndex((neighbor) => neighbor >= tree))
+    .length + 1
+);
+
+const multiply = (first: number, second: number) => first * second;
+
+const descending = (previous: number, next: number) => next - previous;
+
+const getHighestScenicScore = (trees: number[][]) => {
+  const scenicScores: number[] = [];
+  const columns = rotateClockwise(trees);
+
+  trees.forEach((row, rowIndex) => {
+    row.forEach((tree, colIndex) => {
+      if (isEdgeTree(rowIndex, colIndex, trees)) return;
+
+      const score = [
+        ...getRowNeighbors(row, colIndex),
+        ...getColumnNeighbors(
+          columns[colIndex],
+          rowIndex,
+        ),
+      ]
+        .map((neighbors) => visibleNeighborTrees(neighbors, tree))
+        .reduce(multiply);
+
+      scenicScores.push(score);
+    });
+  });
+  return scenicScores.sort(descending).shift()!;
+};
+
 
 const part2 = (rawInput: string) => {
   const input = parseInput(rawInput);
 
-  return;
+  const forest = input.split("\n")
+    .map(rows => rows.split('')
+      .map(Number));
+
+  const highestScenicScore = getHighestScenicScore(forest)
+
+  return highestScenicScore.toString();
 };
 
 run({
   part1: {
     tests: [
-     {
-      input: `
+      {
+        input: `
 30373
 25512
 65332
 33549
 35390
 `,
-expected: "21"
-     }
+        expected: "21"
+      }
     ],
     solution: part1,
   },
