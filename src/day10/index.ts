d3a035dda4cc6e4cfa5c62f580ea1866eb09ca97
Julien Gracia <gracia.julien@gmail.com>
2022-12-10 14:18:55 +0100

    day 10

---
+++ src/day10/index.ts
@@ -0,0 +1,251 @@
+import { chunk_ } from "@arrows/array"
+import run from "aocrunner"
+
+type Command = { cmd: "noop" } | { cmd: "addx"; val: number }
+
+const parseInput = (rawInput: string) =>
+  rawInput.split("\n").map((line) => {
+    const [cmd, val] = line.split(" ")
+    return (
+      cmd === "noop" ? { cmd: cmd } : { cmd: cmd, val: Number(val) }
+    ) as Command
+  })
+
+const part1 = (rawInput: string) => {
+  const input = parseInput(rawInput)
+  let register = 1
+  let cycle = 0
+  let signalStrengthSum = 0
+
+  const nexCycle = () => {
+    cycle++
+
+    if (cycle >= 20 && (cycle - 20) % 40 === 0) {
+      signalStrengthSum += cycle * register
+    }
+  }
+
+  for (const item of input) {
+    if (item.cmd === "noop") {
+      nexCycle()
+    } else {
+      nexCycle()
+      nexCycle()
+      register += item.val
+    }
+  }
+
+  return signalStrengthSum
+}
+
+const part2 = (rawInput: string) => {
+  const input = parseInput(rawInput)
+  let register = 1
+  let cycle = 0
+  let screen: string[] = []
+
+  const nexCycle = () => {
+    cycle++
+
+    const pixelPosInLine = (cycle - 1) % 40
+
+    screen.push(
+      pixelPosInLine >= register - 1 && pixelPosInLine <= register + 1
+        ? "#"
+        : " ",
+    )
+  }
+
+  for (const item of input) {
+    if (item.cmd === "noop") {
+      nexCycle()
+    } else {
+      nexCycle()
+      nexCycle()
+      register += item.val
+    }
+  }
+
+  const result = chunk_(40, screen)
+  .map((line) => line.join(""))
+  .join("\n")
+
+  return result;
+}
+
+const testInput = `
+  addx 15
+  addx -11
+  addx 6
+  addx -3
+  addx 5
+  addx -1
+  addx -8
+  addx 13
+  addx 4
+  noop
+  addx -1
+  addx 5
+  addx -1
+  addx 5
+  addx -1
+  addx 5
+  addx -1
+  addx 5
+  addx -1
+  addx -35
+  addx 1
+  addx 24
+  addx -19
+  addx 1
+  addx 16
+  addx -11
+  noop
+  noop
+  addx 21
+  addx -15
+  noop
+  noop
+  addx -3
+  addx 9
+  addx 1
+  addx -3
+  addx 8
+  addx 1
+  addx 5
+  noop
+  noop
+  noop
+  noop
+  noop
+  addx -36
+  noop
+  addx 1
+  addx 7
+  noop
+  noop
+  noop
+  addx 2
+  addx 6
+  noop
+  noop
+  noop
+  noop
+  noop
+  addx 1
+  noop
+  noop
+  addx 7
+  addx 1
+  noop
+  addx -13
+  addx 13
+  addx 7
+  noop
+  addx 1
+  addx -33
+  noop
+  noop
+  noop
+  addx 2
+  noop
+  noop
+  noop
+  addx 8
+  noop
+  addx -1
+  addx 2
+  addx 1
+  noop
+  addx 17
+  addx -9
+  addx 1
+  addx 1
+  addx -3
+  addx 11
+  noop
+  noop
+  addx 1
+  noop
+  addx 1
+  noop
+  noop
+  addx -13
+  addx -19
+  addx 1
+  addx 3
+  addx 26
+  addx -30
+  addx 12
+  addx -1
+  addx 3
+  addx 1
+  noop
+  noop
+  noop
+  addx -9
+  addx 18
+  addx 1
+  addx 2
+  noop
+  noop
+  addx 9
+  noop
+  noop
+  noop
+  addx -1
+  addx 2
+  addx -37
+  addx 1
+  addx 3
+  noop
+  addx 15
+  addx -21
+  addx 22
+  addx -6
+  addx 1
+  noop
+  addx 2
+  addx 1
+  noop
+  addx -10
+  noop
+  noop
+  addx 20
+  addx 1
+  addx 2
+  addx 2
+  addx -6
+  addx -11
+  noop
+  noop
+  noop
+`
+
+run({
+  part1: {
+    tests: [
+      {
+        input: testInput,
+        expected: 13140,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: testInput,
+        expected: `##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....`.replace(/\./g, " "),
+      },
+    ],
+    solution: part2,
+  },
+  // onlyTests: true,
+})
+
